7a69152dc04bef097b5f837228754455099eb554
rajatrawal <72153827+rajatrawal@users.noreply.github.com>
2021-10-01 16:15:14 +0530

    Create fibo.py
    fibonacci series in one function

---
+++ Python/fibo.py
@@ -0,0 +1,7 @@
+#fibonacci series functon in one line
+
+n = int(input('Enter How Many Number Of Fibonacci Series Do You Want :'))
+fib = lambda x: x if x < 2 else fib(x-1) + fib(x-2) 
+
+#how to execute function
+print(list(map(fib,range(1,n+1))))

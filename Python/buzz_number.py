f59c297202ba5830e262c75f5968528e2f8d9b96
Subhadip Sarkar <edusubhadip@gmail.com>
2021-10-18 19:39:46 +0530

    Buzz number program added

---
+++ Python/buzz_number.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number to check for buzz! "))
+if number % 7 == 0 or number % 10 == 7:
+    print("Hurray! Thats a buzz number")
+else:
+    print("Better luck next time")
+
+
+# Created by Subhadip Sarkar on 07-10-21

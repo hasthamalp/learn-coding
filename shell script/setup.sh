656c09504af34ca7d9cc448c9ccb19208736899c
Joseph Jose <71834579+josephjosedev@users.noreply.github.com>
2022-10-01 19:17:36 +0530

    Update setup.sh
    script program  can access only variables and functions defined with keyword export in the parent shell.

--- shell script/setup.sh
+++ shell script/setup.sh
@@ -1,1 +1,13 @@
-ff
+#!/bin/bash
+echo "expVar = $expVar"
+echo "myVar = $myVar"
+export expVar = "Exported variable"
+myVar="My variable"
+
+./test_var
+expVar = Exported variable
+myVar = 
+
+source test_var
+expVar = Exported
+variable myVar = My variable

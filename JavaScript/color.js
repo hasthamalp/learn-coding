351f4e1d57234bea2f910a29ad22673ad9fe74c4
Joseph Jose <71834579+josephjosedev@users.noreply.github.com>
2022-10-01 22:12:25 +0530

    Create color.js
    program to change color

---
+++ JavaScript/color.js
@@ -0,0 +1,5 @@
+document.getElementBiId("your id").style.color = "your color";
+//it can also be used as:
+document.body.style.backgroundColor = "your color";
+var span = document.getElementsByTagName("span")[0];
+span.style.color = "red";
